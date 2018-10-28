08163f1b5abafb2dd00d2021145cfedcda94f305
Andrea Altea <oni.zerone@gmail.com>
2018-10-28 21:32:53 +0100

    Feat: define TableBinderDataSource

--- SOTViewModelRepresenter/Core/Classes/DataStructure/ItemViewModel.swift
+++ SOTViewModelRepresenter/Core/Classes/DataStructure/ItemViewModel.swift
@@ -11,5 +11,5 @@ public protocol ItemViewModel {
     
     var reuseIdentifier: String { get }
     
-    func setup(_ cell: UICollectionReusableView, in collectionView: UICollectionView, at indexPath: IndexPath)
+    func setup(_ view: UIView, in containerView: UIView, at indexPath: IndexPath)
 }
