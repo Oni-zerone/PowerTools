587e9570d06342533361af1af790c356a4bdea36
Andrea Altea <oni.zerone@gmail.com>
2018-11-26 19:39:01 +0100

    Feat: move ViewRatio to GridDescriptor file

--- PowerTools/CollectionVM/Classes/DataStructure/ItemViewModel.swift
+++ PowerTools/CollectionVM/Classes/DataStructure/ItemViewModel.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-public struct ViewRatio: Equatable {
-    
-    public var multiplier: CGFloat
-    
-    public var constant: CGFloat
-    
-    public init(multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
-        self.multiplier = multiplier
-        self.constant = constant
-    }
-    
-    public static let standard = ViewRatio(multiplier: 1.0, constant: 0.0)
-}
-
 public protocol ItemViewDescriptor {
     
     var reuseIdentifier: String { get }
