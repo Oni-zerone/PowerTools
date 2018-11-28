bcedef5dfef772482130bc7a2a1df0f234b1d282
Andrea Altea <oni.zerone@gmail.com>
2018-11-28 23:52:34 +0100

    Feat: avoid necessity of a baseClass with a generic BuilderContainer who will cast it's type

--- PowerTools/CollectionVM/Classes/Delegates/InteractionFactory.swift
+++ PowerTools/CollectionVM/Classes/Delegates/InteractionFactory.swift
@@ -9,39 +9,37 @@ import Foundation
 
 public typealias ControllerBuilder = ItemViewModel & AbstractBuilder
 
-open class BaseViewModel<ParameterType>: NSObject, ItemViewModel, AbstractBuilder {
+protocol ItemViewModelBuilder: ItemViewModel {
+    
+    var shouldPresentModally: Bool { get }
+    
+    func getController<Parameters>(with parameters: Parameters) -> UIViewController?
+}
 
-    public typealias Parameters = ParameterType
+extension ItemViewModelBuilder {
     
-    open var descriptor: ItemViewDescriptor
-    
-    open func setup(_ view: UIView, in containerView: UIView, at indexPath: IndexPath) {
-        
-    }
-    
-    open func build(_ parameters: ParameterType) -> UIViewController? {
-        return nil
-    }
-    
-    public init(descriptor: ItemViewDescriptor) {
-        self.descriptor = descriptor
+    var shouldPresentModally: Bool {
+        return false
     }
 }
 
-protocol InteractionFactory: AbstractFactory, InteractionDelegate {
-    
-}
-
-extension InteractionFactory {
+extension InteractionDelegate where Self: AbstractFactory {
     
     public func containerView(_ containerView: UIView, shouldSelect item: ItemViewModel) -> Bool {
-        return item is BaseViewModel<Parameters>
+        return item is ItemViewModelBuilder
     }
     
     public func containerView(_ containerView: UIView, didSelect item: ItemViewModel) {
-        guard let builder = item as? BaseViewModel<Parameters> else {
+        
+        guard let itemBuilder = item as? ItemViewModelBuilder,
+            let controller = itemBuilder.getController(with: self.parameters) else {
+                return
+        }
+        
+        if itemBuilder.shouldPresentModally {
+            self.presenterViewController?.present(controller, animated: true, completion: nil)
             return
         }
-        self.showVC(from: builder, sender: self)
+        self.presenterViewController?.show(controller, sender: self)
     }
 }
