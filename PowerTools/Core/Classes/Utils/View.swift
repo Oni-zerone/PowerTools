866c45c92523fa0ce6e2b7316646db9828ea570a
Andrea Altea <oni.zerone@gmail.com>
2018-11-29 17:54:30 +0100

    Feat: Explode Attach in directions

---
+++ PowerTools/Core/Classes/Utils/View.swift
@@ -0,0 +1,49 @@
+//
+//  View.swift
+//  PowerTools
+//
+//  Created by Andrea Altea on 29/11/2018.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    enum AttachPosition {
+        
+        case fill
+        case top
+        case right
+        case left
+        case bottom
+    }
+    
+    public func attach(view: UIView,  in position: AttachPosition) {
+        
+        self.verticalAttach(view: view, in: position)
+        self.horizonalAttach(view: view, in: position)
+    }
+    
+    private func verticalAttach(view: UIView, in position: AttachPosition) {
+        
+        if position != .right {
+            self.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        }
+        
+        if position != .left {
+            self.rightAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        }
+    }
+    
+    private func horizonalAttach(view: UIView, in position: AttachPosition) {
+
+        if position != .top {
+            self.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        }
+        
+        if position != .bottom {
+            self.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        }
+
+    }
+}
