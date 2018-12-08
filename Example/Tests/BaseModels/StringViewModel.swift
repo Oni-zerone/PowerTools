202479cd73346fe75eede2ec58306bf8b2689e44
Andrea Altea <oni.zerone@gmail.com>
2018-12-08 02:01:25 +0100

    Feat: add insertionTest

---
+++ Example/Tests/BaseModels/StringViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  StringViewModel.swift
+//  PowerTools_Tests
+//
+//  Created by Andrea Altea on 08/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+@testable import PowerTools
+
+struct StringDescriptor: ItemViewDescriptor {
+
+    var reuseIdentifier: String
+}
+
+struct StringViewModel: ItemViewModel, Hashable {
+    
+    var descriptor: ItemViewDescriptor
+    
+    var string: String {
+        return self.descriptor.reuseIdentifier
+    }
+    
+    init(string: String) {
+        self.descriptor = StringDescriptor(reuseIdentifier: string)
+    }
+    
+    func setup(_ view: UIView, in containerView: UIView, at indexPath: IndexPath) {
+        
+    }
+    
+    static func == (lhs: StringViewModel, rhs: StringViewModel) -> Bool {
+        return lhs.string == rhs.string
+    }
+    
+    var hashValue: Int {
+        return self.string.hashValue
+    }
+}
+
+extension String {
+    
+    var vm: StringViewModel {
+        return StringViewModel(string: self)
+    }
+}
---
+++ Example/Tests/ModelUpdateTests/ModelUpdateTests.swift
@@ -0,0 +1,46 @@
+//
+//  ModelUpdateTests.swift
+//  PowerTools_Tests
+//
+//  Created by Andrea Altea on 08/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import PowerTools
+
+class ModelUpdateTests: XCTestCase {
+
+    var baseModel: [StringSectionViewModel]!
+    
+    override func setUp() {
+        self.baseModel = ["abc".sectionVM]
+    }
+
+    override func tearDown() {
+        self.baseModel = nil
+    }
+
+    func testModelInsertion() {
+        guard var mutatedModel = self.baseModel else {
+            return XCTFail("Invalid model")
+        }
+        mutatedModel[0].items.append("d".vm)
+        
+        let changes = ModelUpdate(from: self.baseModel, to: mutatedModel)
+        XCTAssert(changes.model as? [StringSectionViewModel] == mutatedModel)
+        XCTAssert(changes.change?.count == 1)
+        guard let change = changes.change?.first else {
+            return XCTFail("Invalid change")
+        }
+        
+        switch change {
+            
+        case .insert(let newPosition):
+            XCTAssert(IndexPath(item: 3, section: 0) == newPosition)
+            
+        default:
+            XCTFail("Invalid position")
+        }
+    }
+}
