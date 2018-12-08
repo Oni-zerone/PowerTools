0109fb6d78d97f3fd7aae0d453db14f2b39a664f
Andrea Altea <oni.zerone@gmail.com>
2018-12-08 12:20:35 +0100

    Feat: add removal in the middle test

--- Example/Tests/ModelUpdateTests/ModelUpdateTests.swift
+++ Example/Tests/ModelUpdateTests/ModelUpdateTests.swift
@@ -45,6 +45,21 @@ class ModelUpdateTests: XCTestCase {
         XCTAssert(changes.change!.first!.isDelete(IndexPath(item: 2, section: 0)))
     }
 
+    func testModelDeletion2() {
+        guard var mutatedModel = self.baseModel else {
+            return XCTFail("Invalid model")
+        }
+        mutatedModel[0].items.remove(at: 1)
+        
+        let changes = ModelUpdate(from: self.baseModel, to: mutatedModel)
+        XCTAssert(changes.model as? [StringSectionViewModel] == mutatedModel)
+        XCTAssert(changes.change?.count == 2)
+        XCTAssert(changes.change!.contains(where: { $0.isDelete(IndexPath(item: 1, section: 0)) }))
+        XCTAssert(changes.change!.contains(where: {
+            $0.isMove(IndexPath(item: 2, section: 0), IndexPath(item: 1, section: 0))
+        }))
+    }
+
     func testModelReplacement() {
         guard var mutatedModel = self.baseModel else {
             return XCTFail("Invalid model")
