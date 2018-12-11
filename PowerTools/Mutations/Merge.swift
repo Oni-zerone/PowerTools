e9f68e1fba5151b8222ed0e37c23bb328417ac1f
Andrea Altea <oni.zerone@gmail.com>
2018-12-11 01:02:56 +0100

    Feat: add base merge modes

--- PowerTools/Mutations/Merge.swift
+++ PowerTools/Mutations/Merge.swift
@@ -14,6 +14,49 @@ public struct Merge<Element> {
     public var action: Action
 }
 
+public extension Merge {
+    
+    public static var injectBefore: Merge<Element> {
+        
+        return Merge { (first, second) -> [Element] in
+            var mutableFirst = first
+            mutableFirst.insert(contentsOf: second, at: 0)
+            return mutableFirst
+        }
+    }
+    
+    public static var injectAfter: Merge<Element> {
+        
+        return Merge { (items, contents) -> [Element] in
+            var mutableItems = items
+            mutableItems.append(contentsOf: contents)
+            return mutableItems
+        }
+    }
+    
+    public static func inject(at index: Int = 0) -> Merge<Element> {
+        
+        return Merge { (first, second) -> [Element] in
+            var mutableFirst = first
+            let splitIndex = max(0, min(first.count, index))
+            mutableFirst.insert(contentsOf: second, at: splitIndex)
+            return mutableFirst
+        }
+    }
+    
+    public static func inject(offset: Int, frequency: Int) -> Merge<Element> {
+        
+        return Merge { (first, second) -> [Element] in
+            var mutableFirst = first
+            (0 ..< second.count).forEach{ index in
+                let safeIndex = max(0, min((index * frequency) + offset, mutableFirst.count))
+                mutableFirst.insert(second[index], at: safeIndex)
+            }
+            return mutableFirst
+        }
+    }
+}
+
 public extension Array {
     
     public func merge(contents: [Element], mode: Merge<Element>) -> [Element] {
