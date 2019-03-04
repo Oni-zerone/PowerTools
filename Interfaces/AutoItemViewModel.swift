3521511e7e8559e950a0a5c6484ea1b01a7de071
Andrea Altea <oni.zerone@gmail.com>
2019-03-04 14:01:20 +0100

    Feat: add AutoItemViewModel protocol and define sourcery template

---
+++ Interfaces/AutoItemViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  AutoItemViewModel.swift
+//  PowerTools
+//
+//  Created by Andrea Altea on 04/03/2019.
+//
+
+import Foundation
+
+/// Defines an ItemViewModel automatically compliant to Hashable and Equatable protocols
+protocol AutoItemViewModel: ItemViewModel { }
