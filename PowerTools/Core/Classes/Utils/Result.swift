2c95ba7954dd59d2b32336cf51f4657ab68b0ce5
Andrea Altea <oni.zerone@gmail.com>
2019-05-11 11:58:58 +0200

    Feat: add map and success/failure block handling for Result type

---
+++ PowerTools/Core/Classes/Utils/Result.swift
@@ -0,0 +1,44 @@
+//
+//  Result.swift
+//  Pods-PowerTools_Example
+//
+//  Created by Andrea Altea on 11/05/2019.
+//
+
+import Foundation
+
+public extension Result {
+    
+    func map<V>(_ transform: (Success) -> V) -> Result<V, Failure> {
+        switch self {
+        
+        case let .failure(error):
+            return .failure(error)
+            
+        case let .success(value):
+            return .success((transform(value)))
+        }
+    }
+ 
+    struct FailedResult<Failure> {
+        
+        fileprivate let failure: Failure?
+        
+        public func failed(_ operation: @escaping (Failure) -> Void) {
+            guard let failure = self.failure else { return }
+            operation(failure)
+        }
+    }
+    
+    @discardableResult
+    func done(_ operation: @escaping (Success) -> Void) -> FailedResult<Failure> {
+        
+        switch self {
+        case let .success(value):
+            operation(value)
+            return FailedResult(failure: nil)
+        case let .failure(failure):
+            return FailedResult(failure: failure)
+        }
+    }
+}
