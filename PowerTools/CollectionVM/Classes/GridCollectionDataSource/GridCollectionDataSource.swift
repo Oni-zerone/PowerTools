785f71769b60e47a69f797d46faaebfe1df984aa
Andrea Altea <oni.zerone@gmail.com>
2019-05-22 01:22:10 +0200

    Refactor: Split  GridCollectionDataSource

--- PowerTools/CollectionVM/Classes/GridCollectionDataSource/GridCollectionDataSource.swift
+++ PowerTools/CollectionVM/Classes/GridCollectionDataSource/GridCollectionDataSource.swift
@@ -190,30 +190,3 @@ public class GridCollectionDataSource: CollectionBinderDataSource, UICollectionV
         return self.scrollViewDelegate?.viewForZooming?(in: scrollView)
     }
 }
-
-internal extension GridCollectionDataSource {
-    
-    func module(for section: Int, in collection: UICollectionView) -> GridModule? {
-        
-        if let module = self.moduleCache[section] {
-            return module
-        }
-        
-        guard self.model.indices.contains(section),
-            let sectionViewModel = self.model[section] as? GridSection else {
-            return nil
-        }
-        let module = sectionViewModel.module(for: collection)
-        self.moduleCache[section] = module
-        return module
-    }
-    
-    func referenceSizeOf(elementOfKind kind: String, for section: Int, in collectionView: UICollectionView) -> CGSize {
-        
-        guard let module = self.module(for: section, in: collectionView),
-            let header = self.model.viewModel(ofKind: kind, section: section) as? GridItem else {
-                return .zero
-        }
-        return header.size(in: collectionView, module: module)
-    }
-}
